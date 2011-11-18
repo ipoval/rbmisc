74413aabf289037b02d8a2668f548f6b42273066
Ivan Povalyukhin <ipoval@ya.ru>
2011-11-17 16:51:19 -0800

    small refactoring using -> operator

--- spec/rubymisc/exceptional_spec.rb
+++ spec/rubymisc/exceptional_spec.rb
@@ -5,31 +5,29 @@ require 'spec_helper'
 describe Rubymisc::Exceptional do
   describe '#ASSERT' do
     specify 'catches failing block and raises RuntimeError' do
-      expect { ASSERT { fail 'Fail message!' } }.to raise_error RuntimeError
+      failing_proc = -> { fail 'Fail message!' }
+      expect { ASSERT &failing_proc }.to raise_error RuntimeError
     end
 
     specify 'ok block does not raise error' do
-      expect { ASSERT { nil } }.to_not raise_error
+      success_proc = -> { true }
+      expect { ASSERT &success_proc }.to_not raise_error
     end
   end
 
   describe '#errors_with_message' do
     specify 'rescue an exception if message match' do
-      expect {
-        begin
-          fail 'Timeout socket'
-        rescue errors_with_message(/socket/)
-        end
-      }.to_not raise_exception
+      e_socket = -> {
+        begin; fail 'Timeout socket'; rescue errors_with_message(/socket/); end
+      }
+      expect(&e_socket).to_not raise_exception
     end
 
     specify 'do not rescue an exception if no message match' do
-      expect {
-        begin
-          fail 'Timeout without correct token'
-        rescue errors_with_message(/socket/)
-        end
-      }.to raise_exception(RuntimeError, 'Timeout without correct token')
+      e_none_socket = -> {
+        begin; fail 'Timeout without correct token'; rescue errors_with_message(/socket/); end
+      }
+      expect(&e_none_socket).to raise_exception(RuntimeError, 'Timeout without correct token')
     end
   end
 
