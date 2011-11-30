8303c8802ea8898cb20f599da0cdcea81de5873e
Ivan Povalyukhin <ipoval@ya.ru>
2011-11-30 14:46:12 -0800

    added Object#not Functor

--- lib/rubymisc/ext/object.rb
+++ lib/rubymisc/ext/object.rb
@@ -5,6 +5,18 @@ module Rubymisc
     def in?(collection)
       collection.respond_to?(:include?) ? collection.include?(self) : false
     end
+
+    def not
+      not_functor = lambda { |op, *a, &b| !self.__send__(op, *a, &b) }
+
+      not_functor.singleton_class.class_eval <<-CODE
+        def method_missing(method, *arguments, &block)
+          call(method, *arguments, &block)
+        end
+      CODE
+
+      not_functor
+    end
   end
 end
 
--- spec/rubymisc/ext/object_spec.rb
+++ spec/rubymisc/ext/object_spec.rb
@@ -3,9 +3,9 @@
 require 'spec_helper'
 
 describe Rubymisc::Object do
-  describe '#in?' do
-    let(:collection) { [1, 2, 3, 4, 5, 100] }
+  let(:collection) { [1, 2, 3, 4, 5, 100] }
 
+  describe '#in?' do
     context 'given collection includes element' do
       specify 'returns true' do
         100.in?(collection).should be_true
@@ -16,6 +16,18 @@ describe Rubymisc::Object do
       specify 'returns false' do
         101.in?(collection).should be_false
       end
+    end
+  end
+
+  describe '#not Functor' do
+    specify 'String#empty? returns false for blank string with #not Functor' do
+      ''.empty?.should be true
+      ''.not.empty?.should be false
+    end
+
+    specify 'Enumerable#include? returns false for collection having an element with #not Functor' do
+      collection.include?(1).should be true
+      collection.not.include?(1).should be false
     end
   end
 end
